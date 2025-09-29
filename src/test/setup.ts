6ce708b5be5ec12506f1dcbd282f34d5b66f3322
Ignat Ignatov <i@ignat.dev>
2025-09-29 16:23:40 +0300

    Make the testing library's output less verbose.

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,5 +1,17 @@
 import "@testing-library/jest-dom"
+import { configure } from "@testing-library/react"
 import type { ReactNode } from "react"
+
+configure({
+  getElementError: (message: string | null) => {
+    const error = new Error(message ?? "Unexpected error occurred.")
+
+    error.name = "TestingLibraryElementError"
+    error.stack = undefined
+
+    return error
+  }
+})
 
 vi.mock("@/lib/i18n", () => ({
   I18nProvider: ({ children }: { children: ReactNode }) => children,
