298e1e8517f4932a532868287efa0bf2d4d5a09d
Ignat Ignatov <i@ignat.dev>
2025-09-29 21:49:22 +0300

    Add some example UI translations.

--- src/lib/i18n/constants.ts
+++ src/lib/i18n/constants.ts
@@ -5,6 +5,6 @@ import { I18nContextValue } from "./types"
 
 export const I18nContext = createContext<I18nContextValue | null>(null)
 
-export const SUPPORTED_LOCALES: Array<string> = [ "en" ]
+export const SUPPORTED_LOCALES: Array<string> = [ "de", "en", "es" ]
 
 export const DEFAULT_LOCALE: typeof SUPPORTED_LOCALES[number] = "en"
